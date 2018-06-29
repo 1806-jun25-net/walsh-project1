cc6d76b89adbcae2dbfa5252ba06179088f9fef2
Angelo <angelo@Nunya.local>
2018-06-29 18:15:48 -0400

    added private members to User class

--- User.cs
+++ User.cs
@@ -1,2 +1,17 @@
 using System;
 
+public class User 
+{
+	private string FirstName {set; get; }
+	private string LastName {set; get; }
+	private string PreferredLocation {set; get; }
+	
+	private Queue<Set<string>> OrderHistory = "";
+	
+	
+	
+
+
+
+}
+
