789f1c7a583a4d9655bed5b71bbe599ad43fcbad
AngeloWalsh <1427856+AngeloWalsh@users.noreply.github.com>
2018-06-30 12:36:37 -0700

    added menu of display options to show user input options

---
+++ Project1/PizzaProject/PizzaProject.UI/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaProject.UI
+{
+    public class Menu
+    {
+        
+        // display menu
+        public void ShowMenu()
+        {
+            Console.WriteLine("Welcome to Pizza Pizza!\n");
+            Console.WriteLine("1: Place Order");
+            Console.WriteLine("2: Get Suggest Order");
+            Console.WriteLine("3. Search Users");
+            Console.WriteLine("4. Display order details");
+            Console.WriteLine("5. Display location order history");
+
+            // This menu will need a sub menu  
+            Console.WriteLine("6. Display sorted history");
+
+            Console.WriteLine("7. Save all data to XML");
+            Console.WriteLine("8. Load data from disk");
+                
+        }
+
+        // get user input 
+
+        
+    }
+}
--- Project1/PizzaProject/PizzaProject.UI/Program.cs
+++ Project1/PizzaProject/PizzaProject.UI/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
+
 namespace PizzaProject.UI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create menu object and display menu options
+            Menu menu = new Menu();
+
+            menu.ShowMenu();
+
+            
         }
     }
 }
