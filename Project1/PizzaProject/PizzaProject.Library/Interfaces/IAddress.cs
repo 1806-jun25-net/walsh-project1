72942608556947e1a423b01005dd485667d96a77
AngeloWalsh <1427856+AngeloWalsh@users.noreply.github.com>
2018-06-30 20:55:28 -0700

    Added fields to Order class, implements Address interface

---
+++ Project1/PizzaProject/PizzaProject.Library/Interfaces/IAddress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaProject.Library.Interfaces
+{
+    public interface IAddress
+    {
+        string Street { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string ZipCode { get; set; }
+    }
+}
--- Project1/PizzaProject/PizzaProject.Library/Models/Order.cs
+++ Project1/PizzaProject/PizzaProject.Library/Models/Order.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using PizzaProject.Library.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PizzaProject.Library.Models
 {
-    class Order 
+    class Order : IAddress 
     {
+        string IAddress.Street { get; set; }
+        string IAddress.City { get; set; }
+        string IAddress.State { get; set; }
+        string IAddress.ZipCode { get; set; }
+        private DateTime OrderTime { get; set; }
+        private int Quantity { get; set; }          // can't be less than zero or more than 12
+        private int Cost { get; set; }              // can't exceed $500
 
     }
 }
