529a63c5f50f0bc546050d3bd00a688d31186172
AngeloWalsh <1427856+AngeloWalsh@users.noreply.github.com>
2018-07-02 13:20:26 -0400

    overloaded constructer to accept default location. Added setordertime method. added method to check if user already ordered.

--- Project1/PizzaProject/PizzaProject.Library/Models/User.cs
+++ Project1/PizzaProject/PizzaProject.Library/Models/User.cs
@@ -9,28 +9,48 @@ public class User
     public string PhoneNumber { set; get; }
     public string DefaultLocation { set; get; }
     public DateTime OrderTime {set; get; }
-
-    // public bool AllowOrder
+    public bool AlreadyOrdered { set; get; } = false;
     
 
     public SortedSet<DateTime> OrderTimes { set; get; }
     public List<List<string>> OrderHistory { get; set; }
 
+    // enter user first name last name and phone number
     public User(string firstName, string lastName, string phoneNumber)
     {
         FirstName = firstName;
         LastName = lastName;
         PhoneNumber = phoneNumber;
+        
     }
 
+    // enter user first name last name phone number and default location 
+    public User( string firstName, string lastName, string phoneNumber, string location)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+        DefaultLocation = location;
+
+    }
     public void SetLocation(string location)
     {
         DefaultLocation = location;
        
     }
 
+    // set order time
+    public void SetOrderTime(DateTime time)
+    {
+        OrderTime = time;
+    }
 
-     
+     // check if user has already ordered
+     public bool CheckIfPreviousOrdered()
+    {
+        return AlreadyOrdered;
+    }
+
 
 }
 
