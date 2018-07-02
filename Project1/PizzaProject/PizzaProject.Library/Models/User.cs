6e8d6eb5bb89e4bf081175379b5caa551941b845
AngeloWalsh <1427856+AngeloWalsh@users.noreply.github.com>
2018-07-02 13:25:57 -0400

    -whatever

--- Project1/PizzaProject/PizzaProject.Library/Models/User.cs
+++ Project1/PizzaProject/PizzaProject.Library/Models/User.cs
@@ -8,24 +8,24 @@ public class User
     public string LastName { set; get; }
     public string PhoneNumber { set; get; }
     public string DefaultLocation { set; get; }
-    public DateTime OrderTime {set; get; }
+    public DateTime OrderTime { set; get; }
     public bool AlreadyOrdered { set; get; } = false;
-    
+
 
     public SortedSet<DateTime> OrderTimes { set; get; }
     public List<List<string>> OrderHistory { get; set; }
 
     // enter user first name last name and phone number
-    public User(string firstName, string lastName, string phoneNumber)
+    public User( string firstName, string lastName, string phoneNumber )
     {
         FirstName = firstName;
         LastName = lastName;
         PhoneNumber = phoneNumber;
-        
+
     }
 
     // enter user first name last name phone number and default location 
-    public User( string firstName, string lastName, string phoneNumber, string location)
+    public User( string firstName, string lastName, string phoneNumber, string location )
     {
         FirstName = firstName;
         LastName = lastName;
@@ -33,24 +33,23 @@ public class User
         DefaultLocation = location;
 
     }
-    public void SetLocation(string location)
+    public void SetLocation( string location )
     {
         DefaultLocation = location;
-       
+
     }
 
     // set order time
-    public void SetOrderTime(DateTime time)
+    public void SetOrderTime( DateTime time )
     {
         OrderTime = time;
     }
 
-     // check if user has already ordered
-     public bool CheckIfPreviousOrdered()
+    // check if user has already ordered
+    public bool CheckIfPreviousOrdered( )
     {
         return AlreadyOrdered;
     }
-
 
 }
 
