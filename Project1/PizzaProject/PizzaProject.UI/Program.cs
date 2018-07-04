0edc61989d8c6e2e882ebd5ae1463ea4a43b39f3
AngeloWalsh <1427856+AngeloWalsh@users.noreply.github.com>
2018-07-04 00:10:24 -0400

    Removed all methods except for Main in Program.cs

--- Project1/PizzaProject/PizzaProject.UI/Program.cs
+++ Project1/PizzaProject/PizzaProject.UI/Program.cs
@@ -7,9 +7,9 @@ namespace PizzaProject.UI
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main( string[ ] args )
         {
-           
+
             int input;      // stores user menu choice 
 
             // do user choice until they want to exit 
@@ -27,67 +27,12 @@ namespace PizzaProject.UI
                 // call relevant logic
                 Menu.UserInput(input);
 
-                
+
 
 
             } while(input != 9);
 
-         }
-
-        static void ShowMenu()
-        {
-            Console.WriteLine("Welcome to Pizza Pizza!\n");
-            Console.WriteLine("1: Place Order");
-            Console.WriteLine("2: Get Suggest Order");
-            Console.WriteLine("3. Search Users");
-            Console.WriteLine("4. Display order details");
-            Console.WriteLine("5. Display location order history");
-
-            // This menu will need a sub menu  
-            Console.WriteLine("6. Display sorted history");
-
-            Console.WriteLine("7. Save all data to XML");
-            Console.WriteLine("8. Load data from disk");
-            Console.WriteLine("9. Exit");
         }
 
-        // get user input and call relevant logic
-        static void UserInput( int choice )
-        {
-            switch(choice)
-            {
-                case 1:
-                    Console.WriteLine("Call Create user");
-                    break;
-                case 2:
-                    Console.WriteLine("2\n");
-                    break;
-                case 3:
-                    Console.WriteLine("3\n");
-                    break;
-                case 4:
-                    Console.WriteLine("4\n");
-                    break;
-                case 5:
-                    Console.WriteLine("5\n");
-                    break;
-                case 6:
-                    Console.WriteLine("6\n");
-                    break;
-                case 7:
-                    Console.WriteLine("7\n");
-                    break;
-                case 8:
-                    Console.WriteLine("8\n");
-                    break;
-                case 9:
-                    Console.WriteLine("9\n");
-                    Console.WriteLine("Program will now exit!");
-                    break;
-            }
-        }
-
-       
-
-    }
+    }   
 }
