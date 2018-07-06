1783ebf81d1b8a32cf64ba95199cfa04b85f45c2
AngeloWalsh <1427856+AngeloWalsh@users.noreply.github.com>
2018-07-06 06:30:52 -0400

    set up Pizza class with CalculateCost method. Sets TotalCost field.

--- Project1/PizzaProject/PizzaProject.Library/Models/Pizza.cs
+++ Project1/PizzaProject/PizzaProject.Library/Models/Pizza.cs
@@ -11,21 +11,44 @@ namespace PizzaProject.Library.Models
          *           
          * Crust: Small, Medium Large
          */
-        public List<string> Toppings { get; set; } = new List<string>();
-        public string Size { get; set; }
+        public List<string> ToppingsList { get; set; }
         public double TotalCost { get; set; }
-       
-
+        public string CrustSize { get; set; }
 
         // set toppings and crust
-        public Pizza( List<string> tops, string crust )
+        public Pizza( string crust, List<string> tops )
         {
-            Size = crust;
-            Toppings = tops;
+            CrustSize = crust;
+            ToppingsList = tops;
 
         }
 
-        public 
+        // calculates total cost of pizza from crust size and types of toppings
+        // sets class member TotalCost
+        public void CalculateCost( )
+        {
+            int total = 0;
+
+            // cost from crust
+            if(CrustSize == "small")
+                total += 6;
+            else if(CrustSize == "medium")
+                total += 8;
+            else
+                total += 10;
+
+            // cost from toppings. Sauce and cheese do not add more to the price
+            foreach(string item in ToppingsList)
+            {
+                if(item == "Pepperoni")
+                    total += 1;
+
+                if(item == "Sausage")
+                    total += 1;
+            }
+
+            TotalCost = total;
+        }
     }
 }
   
