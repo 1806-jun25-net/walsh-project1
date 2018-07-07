7750511f21a8d6586c5e5c4281f41ec6553f320b
AngeloWalsh <1427856+AngeloWalsh@users.noreply.github.com>
2018-07-07 10:00:54 -0400

    ValidateMenuChoice fully working. Does not accept strings that cant be converted to int

--- Project1/PizzaProject/PizzaProject.UI/Program.cs
+++ Project1/PizzaProject/PizzaProject.UI/Program.cs
@@ -18,17 +18,54 @@ namespace PizzaProject.UI
                 // display menu option 
                 Menu.ShowMenu();
 
-                // get user input
-                Console.WriteLine("Enter a command to choose menu or type 'exit' to quit...");
-                input = int.Parse(Console.ReadLine());
 
-                // validate input
-                // ValidateCharInput()
+                // get user input and validate
+                input = ValidateMenuChoice();
+
                 // call relevant logic
                 Menu.UserInput(input);
 
             } while(input != 9);
 
+        }
+
+        // Gets user input as a string and then checks that a proper integer from menu was inputted
+        static int ValidateMenuChoice()
+        {
+           
+            Console.WriteLine("Enter a command to choose menu or type 'exit' to quit...");
+            string input = Console.ReadLine();
+
+            bool integerInput = false;       // for tryParse to test if string can be converted to int
+            int choice;                     // convert user input string to integer
+
+            do
+            {   
+
+                // check if integer value
+                integerInput = int.TryParse(input, out choice);
+
+                // if non integer value, issue error message and prompt user to re-enter value. 
+                // If integer is entered continues to while loop for range of values accepted
+                if(!integerInput)
+                {
+                    Console.Write("That was not a number. Please enter integers only from 1 to 9: ");
+                    input = Console.ReadLine();
+                }
+                else
+                    integerInput = true;
+                
+            } while(!integerInput);
+         
+           
+            // validate integer from  1 to 9
+            while (choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 5 && choice != 6 && choice != 7 && choice != 8 && choice != 9)
+            {
+                Console.Write("Error. You must enter an integer from 1 - 9: ");
+                choice = int.Parse(Console.ReadLine());
+            }
+
+            return choice;
         }
 
     }   
