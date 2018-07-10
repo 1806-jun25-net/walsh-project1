e6716d76a18281f036e54156fd4897fb8b3f32e3
AngeloWalsh <1427856+AngeloWalsh@users.noreply.github.com>
2018-07-09 20:14:24 -0400

    deleted interface folder and scaffolded PizzaDB into Data folder

---
+++ Project1/PizzaProject/PizzaProject.Library/Data/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaProject.Library
+{
+    public partial class Pizza
+    {
+        public int PizzaId { get; set; }
+        public int? Small { get; set; }
+        public int? Medium { get; set; }
+        public int? Large { get; set; }
+        public int? Pepperoni { get; set; }
+        public int? Sausage { get; set; }
+        public int? Cheese { get; set; }
+        public int? Sauce { get; set; }
+    }
+}
