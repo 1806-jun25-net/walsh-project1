5d8e904b0e8e9d0a27b18797ba6fa133b150f95a
AngeloWalsh <1427856+AngeloWalsh@users.noreply.github.com>
2018-07-12 05:36:05 -0400

    Scaffold with just User and Pizza table. Attempt to write to database

---
+++ Project1/PizzaProject/PizzaProject.Data/PizzaDBContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PizzaProject.Data
+{
+    public partial class PizzaDBContext : DbContext
+    {
+        public PizzaDBContext()
+        {
+        }
+
+        public PizzaDBContext(DbContextOptions<PizzaDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Pizza> Pizza { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:walsh-1806.database.windows.net,1433;Initial Catalog=PizzaDB;Persist Security Info=False;User ID=walsh;Password=Aw4215aw!@#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pizza>(entity =>
+            {
+                entity.ToTable("Pizza", "Pizzas");
+
+                entity.Property(e => e.PizzaId).HasColumnName("PizzaID");
+            });
+
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.ToTable("Users", "Pizzas");
+
+                entity.Property(e => e.UsersId).HasColumnName("UsersID");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+        }
+    }
+}
---
+++ Project1/PizzaProject/PizzaProject.Data/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaProject.Data
+{
+    public partial class Users
+    {
+        public int UsersId { get; set; }
+        public int DefaultLocation { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
